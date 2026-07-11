fda934867abce8651879fc1204b76f6c7ca6e17d
MaxiFlores717 <103212606+MaxiFlores717@users.noreply.github.com>
2026-07-11 01:59:47 -0300

    prueba

---
+++ apps/backend/src/users/entities/user.entity.ts
@@ -0,0 +1,12 @@
+export class User {
+    id!: number;
+    nombre!: number;
+    apellido!: number;
+    email!: number;
+    contrasena!: number;
+    telefono!: number;
+    avatar!: number;
+    estado!: number;
+    fechaRegistro!: number;
+    ultimoLogin!: number;
+}
